d20ef4c5d9e894de6d1d16f82073792abbce7650
like5188 <260864866@qq.com>
2020-01-02 22:16:35 +0800

    完善

--- banner/src/main/java/com/like/banner/BannerViewPager.kt
+++ banner/src/main/java/com/like/banner/BannerViewPager.kt
@@ -19,6 +19,10 @@ open class BannerViewPager(context: Context, attrs: AttributeSet?) :
         val a = context.obtainStyledAttributes(attrs, R.styleable.BannerViewPager)
         mHeightWidthRatio = a.getFloat(R.styleable.BannerViewPager_height_width_ratio, 0f)
         a.recycle()
+        // 必须设置这个，否则在使用setPageTransformer()并配合android:clipChildren="false"来使用时，
+        // 会发现由于没有缓存，导致每次都要初始化下一页，从而使得下一页的页面每次都是初始状态，不能达到setPageTransformer()的效果。
+        // 如果有缓存的话，那么setPageTransformer()的动画效果就会作用于缓存的页面，从而正确显示效果。
+        offscreenPageLimit = 3
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
