bf02d4926450c544f51411b7e61a67e3fbc414cd
like5188 <260864866@qq.com>
2022-07-16 22:37:06 +0800

    完善

--- banner/src/main/java/com/like/banner/indicator/IBannerIndicator.kt
+++ banner/src/main/java/com/like/banner/indicator/IBannerIndicator.kt
@@ -1,18 +1,16 @@
 package com.like.banner.indicator
 
-import androidx.viewpager.widget.ViewPager
-
 /**
  * Banner 的指示器基类。
  */
-interface IBannerIndicator : ViewPager.OnPageChangeListener {
+interface IBannerIndicator {
 
-    override fun onPageSelected(position: Int) {
+    fun onPageSelected(position: Int) {
     }
 
-    override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+    fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
     }
 
-    override fun onPageScrollStateChanged(state: Int) {
+    fun onPageScrollStateChanged(state: Int) {
     }
 }
