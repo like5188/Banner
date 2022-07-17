2e0057e3629acee60b120b9bf8a1934349fc9492
like5188 <260864866@qq.com>
2022-07-17 10:56:52 +0800

    完善

--- app/src/main/java/com/like/banner/sample/MyDataSource.kt
+++ app/src/main/java/com/like/banner/sample/MyDataSource.kt
@@ -45,9 +45,6 @@ class MyDataSource : PageNoKeyedPagingDataSource<List<IRecyclerViewItem>?>(0, 10
         val bannerInfo = BannerInfo()
         when (j++) {
             0 -> {
-                bannerInfo.bannerList.add(BannerInfo.Banner("https://www.wanandroid.com/blogimgs/c7a55d24-6fc1-4eb5-97c4-ee1ae694d175.png"))
-                bannerInfo.bannerList.add(BannerInfo.Banner("https://www.wanandroid.com/blogimgs/90c6cc12-742e-4c9f-b318-b912f163b8d0.png"))
-                bannerInfo.bannerList.add(BannerInfo.Banner("https://www.wanandroid.com/blogimgs/62c1bd68-b5f3-4a3c-a649-7ca8c7dfabe6.png"))
             }
             1 -> {
                 bannerInfo.bannerList.add(BannerInfo.Banner("https://mall02.sogoucdn.com/image/2019/03/18/20190318094408_4590.png"))
