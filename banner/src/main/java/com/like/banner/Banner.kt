2ca43392d9d8dd8bc4303c8e2d33a5e221ef3ecc
like5188 <260864866@qq.com>
2022-07-18 16:48:29 +0800

    更好无限轮播实现方式为前后各加一条数据

--- banner/src/main/java/com/like/banner/Banner.kt
+++ banner/src/main/java/com/like/banner/Banner.kt
@@ -137,6 +137,8 @@ open class Banner(context: Context, attrs: AttributeSet?) : FrameLayout(context,
      * 是否需要 bindViewHolder
      * 1、没有设置adapter。
      * 2、新旧数据不相同。
+     *
+     * 注意：如果不判断直接 bindViewHolder 进行相关设置的话，会导致在复用时，每次都会重新设置，然后显示第一个页面。
      */
     fun <T> needBindViewHolder(newData: List<T>, diffCallback: DiffUtil.ItemCallback<T>): Boolean {
         if (mViewPager2.adapter == null) {
